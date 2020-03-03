af1a4a0739b61a9dc4b82ca872db2d64088b5e96
luaifei <lu.aifei@thoughtworks.com>
2020-03-03 21:49:48 +0800

    Code Smell[过长函数]： 1. 卫语句； 2. 拆分函数。

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -16,59 +16,72 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
+        if (m_score1 == m_score2) {
+            return getDisplayStrWithSameScore();
+        }
+
+        if (m_score1 >= 4 || m_score2 >= 4) {
+            return getDisplayStrWithScoreOver4();
+        }
+
+        return getDefaultDisplayStr();
+    }
+
+    private String getDefaultDisplayStr() {
         StringBuilder score = new StringBuilder();
         int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
+
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempScore = m_score1;
+            } else {
+                score.append("-");
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
                 case 0:
-                    score = new StringBuilder("Love-All");
+                    score.append("Love");
                     break;
                 case 1:
-                    score = new StringBuilder("Fifteen-All");
+                    score.append("Fifteen");
                     break;
                 case 2:
-                    score = new StringBuilder("Thirty-All");
+                    score.append("Thirty");
                     break;
-                default:
-                    score = new StringBuilder("Deuce");
+                case 3:
+                    score.append("Forty");
                     break;
-
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) {
-                score = new StringBuilder("Advantage player1");
-            } else if (minusResult == -1) {
-                score = new StringBuilder("Advantage player2");
-            } else if (minusResult >= 2) {
-                score = new StringBuilder("Win for player1");
-            } else {
-                score = new StringBuilder("Win for player2");
-            }
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) {
-                    tempScore = m_score1;
-                } else {
-                    score.append("-");
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
             }
         }
         return score.toString();
     }
+
+    private String getDisplayStrWithScoreOver4() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) {
+            return "Advantage player1";
+        }
+        if (minusResult == -1) {
+            return "Advantage player2";
+        }
+        if (minusResult >= 2) {
+            return "Win for player1";
+        }
+
+        return "Win for player2";
+    }
+
+    private String getDisplayStrWithSameScore() {
+        switch (m_score1) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
 }
