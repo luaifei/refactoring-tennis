ea4b95b8e1c7a68d925f8fe60fd4d2a9a7fc7263
luaifei <lu.aifei@thoughtworks.com>
2020-03-04 19:14:40 +0800

    Code Smell [过长方法] 1. 卫语句

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -6,11 +6,7 @@ public class TennisGame2 implements TennisGame {
     public int P1point = 0;
     public int P2point = 0;
 
-    public String P1res = "";
-    public String P2res = "";
-
     public String getScore() {
-        String score = "";
         if (P1point == P2point) {
             if (P1point < 3) {
                 return getDescriptionForScore(P1point) + "-All";
@@ -18,61 +14,17 @@ public class TennisGame2 implements TennisGame {
             return "Deuce";
         }
 
-        if (P1point <= 3) {
-            P1res = getDescriptionForScore(P1point);
+        String playerNameWithHighScore = P1point > P2point ? "player1" : "player2";
+
+        if ((P1point >= 4 || P2point >= 4) && Math.abs(P1point - P2point) >= 2) {
+            return "Win for " + playerNameWithHighScore;
         }
 
-        if (P2point == 0) {
-            P2res = "Love";
-            score = P1res + "-" + P2res;
+        if (P1point >= 3 && P2point >= 3) {
+            return "Advantage " + playerNameWithHighScore;
         }
 
-        if (P2point <= 3) {
-            P2res = getDescriptionForScore(P2point);
-        }
-
-        if (P1point == 0) {
-            P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point) {
-            if (P1point < 4 && P1point >= 2) {
-                P1res = getDescriptionForScore(P1point);
-            }
-
-            if (P2point <=2) {
-                P2res = getDescriptionForScore(P2point);
-            }
-            score = P1res + "-" + P2res;
-        }
-
-        if (P2point > P1point) {
-            if (P2point < 4 && P2point >= 2) {
-                P2res = getDescriptionForScore(P2point);
-            }
-
-            if (P1point >= 1 && P1point <= 2) {
-                P1res = getDescriptionForScore(P1point);
-            }
-            score = P1res + "-" + P2res;
-        }
-
-        if (P1point > P2point && P2point >= 3) {
-            score = "Advantage player1";
-        }
-
-        if (P2point > P1point && P1point >= 3) {
-            score = "Advantage player2";
-        }
-
-        if (P1point >= 4 && (P1point - P2point) >= 2) {
-            score = "Win for player1";
-        }
-        if (P2point >= 4 && (P2point - P1point) >= 2) {
-            score = "Win for player2";
-        }
-        return score;
+        return getDescriptionForScore(P1point) + "-" + getDescriptionForScore(P2point);
     }
 
     private String getDescriptionForScore(int score) {
