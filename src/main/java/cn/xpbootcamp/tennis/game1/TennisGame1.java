d903fef97ad638eb80fc3ddaf33542f6e477289a
luaifei <lu.aifei@thoughtworks.com>
2020-03-04 14:38:16 +0800

    Code Smell[霰弹式修改]： 1. 提取公用变量。 2. 统一变量命名为驼峰

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -3,24 +3,27 @@ package cn.xpbootcamp.tennis.game1;
 import cn.xpbootcamp.tennis.TennisGame;
 
 public class TennisGame1 implements TennisGame {
+    private static String PLAYER1 = "player1";
+    private static String PLAYER2 = "player2";
 
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int score1 = 0;
+    private int score2 = 0;
 
     public void wonPoint(String playerName) {
-        if ("player1".equals(playerName)) {
-            m_score1 += 1;
+
+        if (PLAYER1.equals(playerName)) {
+            score1 += 1;
         } else {
-            m_score2 += 1;
+            score2 += 1;
         }
     }
 
     public String getScore() {
-        if (m_score1 == m_score2) {
+        if (score1 == score2) {
             return getDisplayStrWithSameScore();
         }
 
-        if (m_score1 >= 4 || m_score2 >= 4) {
+        if (score1 >= 4 || score2 >= 4) {
             return getDisplayStrWithScoreOver4();
         }
 
@@ -33,10 +36,10 @@ public class TennisGame1 implements TennisGame {
 
         for (int i = 1; i < 3; i++) {
             if (i == 1) {
-                tempScore = m_score1;
+                tempScore = score1;
             } else {
                 score.append("-");
-                tempScore = m_score2;
+                tempScore = score2;
             }
             score.append(getDescriptionForScore(tempScore));
         }
@@ -44,26 +47,26 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getDisplayStrWithScoreOver4() {
-        int minusResult = m_score1 - m_score2;
+        int minusResult = score1 - score2;
         if (minusResult == 1) {
-            return "Advantage player1";
+            return "Advantage " + PLAYER1;
         }
         if (minusResult == -1) {
-            return "Advantage player2";
+            return "Advantage " + PLAYER2;
         }
         if (minusResult >= 2) {
-            return "Win for player1";
+            return "Win for " + PLAYER1;
         }
 
-        return "Win for player2";
+        return "Win for " + PLAYER2;
     }
 
     private String getDisplayStrWithSameScore() {
-        if (m_score1 >= 3) {
+        if (score1 >= 3) {
             return "Deuce";
         }
 
-        return getDescriptionForScore(m_score1) + "-All";
+        return getDescriptionForScore(score1) + "-All";
     }
 
     private String getDescriptionForScore(int score) {
