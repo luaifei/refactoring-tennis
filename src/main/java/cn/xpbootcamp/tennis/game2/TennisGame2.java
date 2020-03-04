907b28d601969fa44f7b20505720ba1ed2a2b696
luaifei <lu.aifei@thoughtworks.com>
2020-03-04 16:14:34 +0800

    Code Smell[过长方法]： 1. 提取公共方法，进行分数和显示的转换

--- src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/game2/TennisGame2.java
@@ -11,78 +11,49 @@ public class TennisGame2 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0) {
-                score = "Love";
+        if (P1point == P2point) {
+            if (P1point < 3) {
+                return getDescriptionForScore(P1point) + "-All";
             }
-            if (P1point == 1) {
-                score = "Fifteen";
-            }
-            if (P1point == 2) {
-                score = "Thirty";
-            }
-            score += "-All";
+            return "Deuce";
         }
-        if (P1point == P2point && P1point >= 3) {
-            score = "Deuce";
+
+        if (P1point > 0 && P1point <= 3) {
+            P1res = getDescriptionForScore(P1point);
         }
 
         if (P1point > 0 && P2point == 0) {
-            if (P1point == 1) {
-                P1res = "Fifteen";
-            }
-            if (P1point == 2) {
-                P1res = "Thirty";
-            }
-            if (P1point == 3) {
-                P1res = "Forty";
-            }
-
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1) {
-                P2res = "Fifteen";
-            }
-            if (P2point == 2) {
-                P2res = "Thirty";
-            }
-            if (P2point == 3) {
-                P2res = "Forty";
-            }
 
+        if (P2point > 0 && P2point <= 3) {
+            P2res = getDescriptionForScore(P2point);
+        }
+
+        if (P2point > 0 && P1point == 0) {
             P1res = "Love";
             score = P1res + "-" + P2res;
         }
 
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2) {
-                P1res = "Thirty";
+        if (P1point > P2point) {
+            if (P1point < 4 && P1point >= 2) {
+                P1res = getDescriptionForScore(P1point);
             }
-            if (P1point == 3) {
-                P1res = "Forty";
-            }
-            if (P2point == 1) {
-                P2res = "Fifteen";
-            }
-            if (P2point == 2) {
-                P2res = "Thirty";
+
+            if (P2point > 0 && P2point <=2) {
+                P2res = getDescriptionForScore(P2point);
             }
             score = P1res + "-" + P2res;
         }
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2) {
-                P2res = "Thirty";
+
+        if (P2point > P1point) {
+            if (P2point < 4 && P2point >= 2) {
+                P2res = getDescriptionForScore(P2point);
             }
-            if (P2point == 3) {
-                P2res = "Forty";
-            }
-            if (P1point == 1) {
-                P1res = "Fifteen";
-            }
-            if (P1point == 2) {
-                P1res = "Thirty";
+
+            if (P1point >= 1 && P1point <= 2) {
+                P1res = getDescriptionForScore(P1point);
             }
             score = P1res + "-" + P2res;
         }
@@ -102,6 +73,20 @@ public class TennisGame2 implements TennisGame {
             score = "Win for player2";
         }
         return score;
+    }
+
+    private String getDescriptionForScore(int score) {
+        switch (score) {
+            case 0:
+                return "Love";
+            case 1:
+                return "Fifteen";
+            case 2:
+                return "Thirty";
+            case 3:
+                return "Forty";
+        }
+        return null;
     }
 
     public void wonPoint(String player) {
