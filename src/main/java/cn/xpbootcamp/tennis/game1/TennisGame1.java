c78968689d953c265dd49837c7acd9511aa95dea
luaifei <lu.aifei@thoughtworks.com>
2020-03-04 21:50:53 +0800

    1. 简单清理

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -15,15 +15,16 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName)) {
             m_score1 += 1;
-        else
+        } else {
             m_score2 += 1;
+        }
     }
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
+        int tempScore;
         if (m_score1 == m_score2) {
             switch (m_score1) {
                 case 0:
@@ -42,15 +43,21 @@ public class TennisGame1 implements TennisGame {
             }
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult == -1) {
+                score = "Advantage player2";
+            } else if (minusResult >= 2) {
+                score = "Win for player1";
+            } else {
+                score = "Win for player2";
+            }
         } else {
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score += "-";
+                if (i == 1) {
+                    tempScore = m_score1;
+                } else {
+                    score = String.format("%s-", score);
                     tempScore = m_score2;
                 }
                 switch (tempScore) {
--- src/main/java/cn/xpbootcamp/tennis/game3/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/game3/TennisGame3.java
@@ -21,18 +21,20 @@ public class TennisGame3 implements TennisGame {
             s = p[p1];
             return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
         } else {
-            if (p1 == p2)
+            if (p1 == p2) {
                 return "Deuce";
+            }
             s = p1 > p2 ? p1N : p2N;
             return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName)) {
             this.p1 += 1;
-        else
+        } else {
             this.p2 += 1;
+        }
 
     }
 
